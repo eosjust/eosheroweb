71a0cbfaee439e2b978308b5e249ac6c27ea847e
eosjust <eosjust@outlook.com>
2018-11-20 11:12:08 +0800

    update

--- src/components/booking.vue
+++ src/components/booking.vue
@@ -1,32 +1,60 @@
 <template>
-  <el-row>
-    <el-row>
-      <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
-        <herocard></herocard>
-      </el-col>
-      <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
-        <herocard></herocard>
-      </el-col>
-      <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
-        <herocard></herocard>
-      </el-col>
-      <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
-        <herocard></herocard>
-      </el-col>
-      <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
-        <herocard></herocard>
-      </el-col>
-      <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
-        <herocard></herocard>
-      </el-col>
-      <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
-        <herocard></herocard>
-      </el-col>
-      <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
-        <herocard></herocard>
-      </el-col>
-
-    </el-row>
+  <el-row type="flex" justify="center">
+    <el-col :span="20">
+      <el-row>
+        <el-row style="padding: 10px;">
+          <el-col :span="10">
+            <el-row type="flex" align="middle" style="height: 60px;">
+              <el-input placeholder="搜索" v-model="searchStr">
+                <el-button slot="append" icon="el-icon-search"></el-button>
+              </el-input>
+            </el-row>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" align="middle" style="height: 60px;">
+              <el-select v-model="sortType" placeholder="排序方式" style="width: 130px;">
+                <el-option label="默认排序" value="1"></el-option>
+                <el-option label="价格从低到高" value="2"></el-option>
+                <el-option label="价格从高到低" value="3"></el-option>
+              </el-select>
+            </el-row>
+          </el-col>
+          <el-col :span="6">
+            <el-row type="flex"  align="middle" style="height: 60px;padding-left: 10px;padding-right: 10px;">
+              <el-checkbox v-model="chkShowMine">只显示我拥有的</el-checkbox>
+            </el-row>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-row>
+            <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
+              <herocard></herocard>
+            </el-col>
+            <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
+              <herocard></herocard>
+            </el-col>
+            <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
+              <herocard></herocard>
+            </el-col>
+            <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
+              <herocard></herocard>
+            </el-col>
+            <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
+              <herocard></herocard>
+            </el-col>
+            <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
+              <herocard></herocard>
+            </el-col>
+            <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
+              <herocard></herocard>
+            </el-col>
+            <el-col :span="this.$store.state.ismobile?12:6" style="padding: 10px;">
+              <herocard></herocard>
+            </el-col>
+          </el-row>
+        </el-row>
+      </el-row>
+    </el-col>
   </el-row>
 </template>
 
@@ -38,6 +66,14 @@
       components: {
         herocard,
       },
+      data(){
+        return{
+          searchStr:'',
+          sortType:'1',
+          chkShowMine:false,
+
+        }
+      }
     }
 </script>
 
--- src/components/herocard.vue
+++ src/components/herocard.vue
@@ -1,32 +1,27 @@
 <template>
-    <el-row style="">
-      <el-row :class="rowclass">
-        <img class="img" src="../assets/001-caocao.jpg" @mouseover="OnCardOver" @mouseout="OnCardOut"/>
+    <el-row style="margin-top: 5px;">
+      <el-row class="card-border">
+        <img :class="buttonimgclass" src="../assets/001-caocao.jpg" @click="OnCardButtonClick" @mouseover="OnCardButtonOver" @mouseout="OnCardButtonOut"/>
         <div style="position: absolute;top: 0px;left: 0px;width: 100%;height: 40px;">
           <el-row type="flex" justify="center" align="middle" style="width: 100%;height: 100%;">
             <div class="card-title light-white">{{ name }}</div>
           </el-row>
         </div>
-
-        <div style="position: absolute;bottom: 0px;left: 0px;width: 100%;height: 30px;background: #00000080;">
-          <el-row type="flex" justify="center" align="middle" style="width: 100%;height: 100%;">
+        <div style="position: absolute;bottom: 0px;left: 0px;width: 100%;height: 30px;background: #00000080;" @mouseover="OnCardButtonOver" @mouseout="OnCardButtonOut">
+          <el-row type="flex" justify="center" align="middle" style="width: 100%;height: 100%;" @mouseover="OnCardButtonOver" @mouseout="OnCardButtonOut">
             <div style="text-align: center;color: wheat">{{ user }}</div>
           </el-row>
         </div>
       </el-row>
-      <el-row type="flex" justify="center" align="middle">
+      <el-row type="flex" justify="center" style="margin-top: 10px;">
         <el-col :span="18">
           <el-row>
-            <el-row type="flex" justify="center" align="middle">
-              <el-row class="card-button" >
-                <img class="img" :src="button2_bg"/>
-                <div style="position: absolute;top: 0px;left: 0px;width: 100%;height: 90%;">
-                  <el-row type="flex" justify="center" align="middle" style="width: 100%;height: 100%;">
-                    <div style="text-align: center;color: whitesmoke">{{ price }}</div>
-                  </el-row>
-                </div>
+            <img :class="buttonimgclass" :src="button2_bg" />
+            <div style="position: absolute;top: 0px;left: 0px;width: 100%;height: 90%;cursor: pointer;" @click="OnCardButtonClick" @mouseover="OnCardButtonOver" @mouseout="OnCardButtonOut">
+              <el-row type="flex" justify="center" align="middle" style="width: 100%;height: 100%;" @mouseover="OnCardButtonOver" @mouseout="OnCardButtonOut">
+                <div style="text-align: center;color: whitesmoke">{{ price }}</div>
               </el-row>
-            </el-row>
+            </div>
           </el-row>
         </el-col>
       </el-row>
@@ -38,7 +33,7 @@
         name: "herocard",
       data() {
         return{
-          rowclass:"card-shadow",
+          buttonimgclass:'card-button-out img',
           button2_bg: require("../assets/button2.png"),
           name:'曹操',
           user:'koibetwallet',
@@ -46,48 +41,45 @@
         }
       },
       methods: {
-        OnCardOver(){
-          this.rowclass='card-shadow-2';
+        OnCardButtonOver(){
+          this.buttonimgclass='card-button-hover img';
         },
-        OnCardOut(){
-          this.rowclass='card-shadow';
+        OnCardButtonOut(){
+          this.buttonimgclass='card-button-out img';
+        },
+        OnCardButtonClick(){
+          alert('aaa');
         }
       }
     }
 </script>
 
 <style scoped>
-
-  .card-button{
-    /*width: 180px;*/
-    height: auto;
-    margin-top: 10px;
-    cursor: pointer;
-  }
   .img {
     width: 100%;
     height: auto;
-  }
-
-  .card-shadow {
-    box-shadow:1px 1px 1px 1px #111111;
     cursor: pointer;
-    border-style: solid;
-    border-color: wheat;
-    border-width: 2px;
   }
-
-  .card-shadow-2 {
-    box-shadow:1px 1px 4px 3px #000000;
-    cursor: pointer;
-    border-style: solid;
-    border-color: wheat;
-    border-width: 2px;
-  }
-
   .card-title {
     color: wheat;
     font-size: 1.4em;
+  }
+  .card-button{
+    height: auto;
+    margin-top: 10px;
+
+  }
+  .card-border {
+    cursor: pointer;
+    border-style: solid;
+    border-color: wheat;
+    border-width: 2px;
+  }
+  .card-button-hover{
+    filter: brightness(1.2);
+  }
+  .card-button-out{
+    filter: brightness(1.0);
   }
   .light-white {
     text-shadow: rgb(43, 0, 43) 1px 1px 5px, rgb(204, 204, 204) 1px 1px 10px, rgb(204, 204, 204) 1px 1px 10px;
